a11d17987621dbadbc7697e5264460424750b4cc
IT20642914 <avishkachanaka@gmail.com>
2024-07-21 11:34:15 +0530

    chore: Add Rectangle1 and Rectangle2 images, and update Login module styles

--- front-end/src/pages/Login/Login.tsx
+++ front-end/src/pages/Login/Login.tsx
@@ -5,6 +5,7 @@ import background from '../../assets/images/background.png'; // Ensure the path 
 import { useNavigate } from 'react-router-dom';
 import {logo} from '../../assets/images';
 import { CustomButton } from '../../assets/theme/theme';
+import { Rectangle1,Rectangle2 } from '../../assets/images';
 const Login: React.FC = () => {
 const navigate =useNavigate();
 
@@ -14,11 +15,16 @@ const navigate =useNavigate();
 
   return (
     <div className={styles.container}>
-             <div className={styles.shape1}></div>
-             <div className={styles.shape2}></div>
-      <Grid container >
-        <Grid item xs={12} md={6} className={styles.loginSection}>
 
+      <Grid container >
+        
+        <Grid item xs={12} md={6} className={styles.loginSection}>
+        <Box className={styles.shape1}>
+        <img src={Rectangle1} alt="" />
+        </Box>
+            <Box className={styles.shape2}>
+            <img src={Rectangle2} alt="" />
+            </Box>
          <Box className={styles.loginBox}>
             <img src={logo} alt="" style={{width:"10rem"}} />
           <Typography  className={styles.subtitle}>
@@ -51,7 +57,9 @@ const navigate =useNavigate();
             <Typography variant="body2">
               Forgot Password?</Typography>
               </Box>
+            
             </Box>
+    
         </Grid>
         <Grid item xs={12} md={6} className={styles.imageSection}>
           <img src={background} alt="Laptop" className={styles.image} />
